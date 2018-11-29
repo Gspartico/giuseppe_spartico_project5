cbd39c99e15e5664619924d9b174a920d2bd505b
Gspartico <gspartico@gmail.com>
2018-11-28 19:42:53 -0500

    first commit, made successful axios call, made button that does axios call on click

--- src/App.js
+++ src/App.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+// const baseURL = `https://api.themoviedb.org/3/`;
+const apiKey = `875e4d600eaf27cc3eaca8c5bf1ac2a8`
+// const apiKey = `f0ba00aa70aa95e488fb89869bf99a39`
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      movie: axios
+    };
+  };
+
+  
+
+  componentDidMount(){
+    return axios({
+      method: 'GET',
+      url: `https://api.themoviedb.org/3/discover/movie`,
+      dataType: 'jsonp',
+      params: {
+        api_key: apiKey,
+        language:'en-US', 
+        sort_by: 'popularity.desc', 
+        include_adult: 'false', 
+        include_video: 'false', 
+        page: 1,
+        with_genres: 28
+
+      }
+    }).then((response) => {
+      response = response.data.results
+      // console.log(response)
+      this.setState({
+        movie: response
+        
+      })
+    })
+      // response = response.data
+      // console.log(response.data.results.genre_ids);
+      // this.state({
+      //   movies: response
+      // })
+}
+handleClick = () =>{
+  console.log('i work')
+  const getMovie = Array.from(this.state.movie)
+  console.log(getMovie)
+  this.setState({
+    response: getMovie
+  })
+//   // this.setState({
+//   //   [e.target.]
+//   // })
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Test button</h1>
+        <button onClick={this.handleClick}>Get a movie</button>
+        
       </div>
     );
   }
 }
 
 export default App;
+
+//Call in Moviedb discover/movies API using axios
+//Isolate the genre_id's in response.results genre_ID"
+
+//link each genre to five different weather options that can be selected by a user
+//when weather option is selected, on click of button randomly pull a movie in the associated array
+//append movie title and image to DOM with results.title and results.poster_path
